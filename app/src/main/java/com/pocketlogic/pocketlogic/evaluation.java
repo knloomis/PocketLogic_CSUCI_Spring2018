c95a53ff2133f62d64cba554976a53e003479f3d
Kira <kira.loomis240@myci.csuci.edu>
2018-04-08 10:55:12 -0700

    Changes to evaluation.java
    Made the activity into a popup, and added an "x" back button in the top corner, so we can have the final win screen access button be hidden until all conditions are satisfied

--- app/src/main/java/com/pocketlogic/pocketlogic/evaluation.java
+++ app/src/main/java/com/pocketlogic/pocketlogic/evaluation.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 /**
@@ -13,11 +16,28 @@ import android.widget.Button;
 
 public class evaluation extends AppCompatActivity {
 
+    double height_modifier = .9;
+    double width_modifier = .9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluation);
         addListeners();
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*width_modifier), (int) (height *height_modifier));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x=0;
+        params.y=-20;
+        getWindow().setAttributes(params);
     }
 
     public void addListeners() {
@@ -29,6 +49,14 @@ public class evaluation extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(context, winScreen.class);
                 startActivity(intent);
+            }
+        });
+
+        Button back = (Button) findViewById(R.id.btn_eval_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
